dd15859c2f4805e46e3123e6df3cb6393ae3bb43
jxStep <strahinja-gajic@protonmail.com>
2023-07-10 22:57:42 +0200

    add static files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,51 +8,63 @@ import (
 	"strconv"
 )
 
+// Home-Handler zum Bedienen der Startseite
 func home(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-        http.NotFound(w, r)
-        return
-    }
+	// Überprüfen, ob der angeforderte Pfad genau "/" ist, 
+	// wenn nicht, sendet die Funktion eine 404 Not Found Antwort an den Benutzer.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 
-    // Include the navigation partial in the template files.
-    files := []string{
-        "./ui/html/base.tmpl.html",
-        "./ui/html/partials/nav.tmpl.html",
-        "./ui/html/pages/home.tmpl.html",
-    }
+	// Liste der Template-Dateien für die Startseite
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
 
-    ts, err := template.ParseFiles(files...)
-    if err != nil {
-        log.Print(err.Error())
-        http.Error(w, "Internal Server Error", 500)
-        return
-    }
+	// Parse die Template-Dateien
+	ts, err := template.ParseFiles(files...)
+	// Wenn ein Fehler beim Parsen auftritt, logge den Fehler und sende eine HTTP 500 Antwort.
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
 
-    err = ts.ExecuteTemplate(w, "base", nil)
-    if err != nil {
-        log.Print(err.Error())
-        http.Error(w, "Internal Server Error", 500)
-    }
+	// Ausführen des Templates, Fehlerbehandlung ähnlich wie beim Parsen
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
+// snippetView-Handler zum Anzeigen eines bestimmten Snippets
 func snippetView(w http.ResponseWriter, r *http.Request) {
-    id, err := strconv.Atoi(r.URL.Query().Get("id"))
-    if err != nil || id < 1 {
-        http.NotFound(w, r)
-        return
-    }
+	// Extrahieren des Snippet-ID Parameters aus dem URL Query String,
+	// bei einem Fehler oder wenn die ID kleiner als 1 ist, wird eine 404 Not Found Antwort gesendet.
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
 
-    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	// Für jetzt senden wir einfach eine Platzhalterantwort zurück
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-
-
+// snippetCreate-Handler zum Erstellen eines neuen Snippets
 func snippetCreate(w http.ResponseWriter, r *http.Request){
+	// Überprüfen ob die Anfrage die Methode POST hat, wenn nicht, senden wir einen '405 Method Not Allowed' Status 
+	// und setzen den 'Allow'-Header auf 'POST'
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
+	// Für jetzt senden wir einfach eine Platzhalterantwort zurück
 	w.Write([]byte("Create a new snippet..."))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,52 +1,72 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
 )
 
 func main() {
-    mux := http.NewServeMux()
+	// Definieren der Standard-Adresse für den HTTP-Server
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
-    fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
-    mux.Handle("/static", http.NotFoundHandler())
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	// Erzeugen eines neuen HTTP Request Multiplexers
+	mux := http.NewServeMux()
 
+	// Erzeugen eines neuen Datei-Servers, der statische Dateien aus dem Ordner "./static" bereitstellt
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
+	// Statische Dateien sind nicht direkt unter "/static" zugänglich
+	mux.Handle("/static", http.NotFoundHandler())
+	// Statische Dateien sind unter "/static/" zugänglich
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Festlegen der Handlerfunktionen für die jeweiligen Pfade
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-    err := http.ListenAndServe(":4000", mux)
-    log.Fatal(err)
+	// Starten des Servers und Ausgeben einer Fehlermeldung, wenn der Server nicht gestartet werden kann
+	log.Printf("Starting Server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	log.Fatal(err)
 }
 
+// Definieren einer neuen Dateisystemstruktur, die das Standard-HTTP-Dateisystem umschließt
 type neuteredFileSystem struct {
-    fs http.FileSystem
+	fs http.FileSystem
 }
 
+// Definieren einer eigenen Open-Funktion für das neuteredFileSystem
 func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-    f, err := nfs.fs.Open(path)
-    if err != nil {
-        return nil, err
-    }
-
-    s, err := f.Stat()
+	// Öffnen der angeforderten Datei
+	f, err := nfs.fs.Open(path)
 	if err != nil {
-    	return nil, err
+		return nil, err
 	}
 
-    if s.IsDir() {
-        index := filepath.Join(path, "index.html")
-        if _, err := nfs.fs.Open(index); err != nil {
-            closeErr := f.Close()
-            if closeErr != nil {
-                return nil, closeErr
-            }
+	// Abrufen der Dateiinformationen
+	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
 
-            return nil, err
-        }
-    }
+	// Überprüfen, ob die angeforderte Datei ein Verzeichnis ist
+	if s.IsDir() {
+		// Wenn ja, versuchen, eine Datei namens "index.html" in diesem Verzeichnis zu öffnen
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			// Schließen der ursprünglich geöffneten Datei und Rückgabe des Fehlers
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
 
-    return f, nil
-}    
+			return nil, err
+		}
+	}
+
+	// Rückgabe der geöffneten Datei, wenn keine Fehler aufgetreten sind
+	return f, nil
+}
